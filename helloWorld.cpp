07083fd4b9dfe7bacb70558811925f56cb122191
Mikhail Rego <mrego1@my.bcit.ca>
2025-01-09 22:02:24 -0800

    Initial commit with helloWorld.cpp

---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+/*
+ * helloWorld.cpp
+ *
+ *  Created on: Jan 9, 2025
+ *      Author: Dwash
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "hello world\n";
+    char x = '0';
+    std::cin >> x;
+    return 0;
+}
+
+
